ee5a760660088ab597edf69ef401422e71488ed3
devdevil-production <saudd2gh@gmail.com>
2020-04-03 13:16:26 +0400

    Add scroll animation when nav link clicked

--- js/main.js
+++ js/main.js
@@ -30,6 +30,29 @@ $(document).ready(function() {
   const navCheckbox = $(".navbar > #nav-checkbox");
 
   navLinks.each((_, link) =>
-    $(link).click(() => navCheckbox.prop("checked", false))
+    $(link).click(function(event) {
+      navCheckbox.prop("checked", false);
+
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $("html, body").animate(
+          {
+            scrollTop: $(hash).offset().top
+          },
+          800,
+          function() {
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          }
+        );
+      } // End if
+    })
   );
 });
